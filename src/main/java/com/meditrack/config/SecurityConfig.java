47e30a3834b79952c92908e20ff3b74e2ec27b99
ElJhonson34 <angel.alvarez20@hotmail.com>
2026-02-20 15:07:06 -0600

    RestController fixed

--- src/main/java/com/meditrack/config/SecurityConfig.java
+++ src/main/java/com/meditrack/config/SecurityConfig.java
@@ -84,14 +84,18 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500",
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
                 "https://meditrackapp.onrender.com"
         ));
 
         configuration.setAllowedMethods(List.of(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
         ));
 
         configuration.setAllowedHeaders(List.of("*"));
